20e06cff51b9738afd65a4ed2849056e036ace33
Jose Perez <font3face@gmail.com>
2022-09-09 10:22:13 -0500

    adding UI first commit

--- logic.js
+++ logic.js
@@ -8,7 +8,7 @@ function getComputerChoice() {
     return options[Math.floor(Math.random() * 3)];
 }
 
-// let computerSelection = getComputerChoice();
+let computerSelection = getComputerChoice();
 
 function getPlayerChoice() {
     let playerAnswer = prompt(
@@ -19,7 +19,7 @@ function getPlayerChoice() {
     return finalSelection;
 }
 
-// let playerSelection = getPlayerChoice();
+let playerSelection = getPlayerChoice();
 
 function playRound(computerSelection, playerSelection) {
     switch (true) {
@@ -55,50 +55,50 @@ function playRound(computerSelection, playerSelection) {
     }
 }
 
-let computerCounter = 0;
-let playerCounter = 0;
+// let computerCounter = 0;
+// let playerCounter = 0;
 
-function Game() {
-    let roundResult = playRound(getComputerChoice(), getPlayerChoice());
+// function game() {
+//     let roundResult = playRound(getComputerChoice(), getPlayerChoice());
 
-    function gameCounter(roundResult) {
-        switch (true) {
-            case roundResult === "A tie, paper!! ":
-                console.log("If you read this, is a tie!!");
-                break;
-            case roundResult === "A tie, rock!!":
-                console.log("If you read this, is a tie!!");
-                break;
-            case roundResult === "A tie, scissors!!":
-                console.log("If you read this, is a tie!!");
-                break;
-            case roundResult === "You lose, Rock beats Scissors!!":
-                computerCounter++;
-                break;
-            case roundResult === "You lose, Paper beats Rock!!":
-                computerCounter++;
-                break;
-            case roundResult === "You Lose, Scissors beats Paper!!":
-                computerCounter++;
-                break;
-            case roundResult === "You Win, Scissors beats Paper!!":
-                playerCounter++;
-                break;
-            case roundResult === "You Win, Paper beats Rock!!":
-                playerCounter++;
-                break;
-            case roundResult === "You Win, Rock beats Scissors!!":
-                playerCounter++;
-                break;
-            default:
-                console.log("Nothing to show");
-        }
-    }
-    gameCounter(roundResult);
-    console.log(playerCounter, computerCounter);
-    if (computerCounter === 5) {
-        console.log("Has perdido manco!!");
-    } else if (playerCounter === 5) {
-        console.log("Eres genial!!!");
-    }
-}
+//     function gameCounter(roundResult) {
+//         switch (true) {
+//             case roundResult === "A tie, paper!! ":
+//                 console.log("If you read this, is a tie!!");
+//                 break;
+//             case roundResult === "A tie, rock!!":
+//                 console.log("If you read this, is a tie!!");
+//                 break;
+//             case roundResult === "A tie, scissors!!":
+//                 console.log("If you read this, is a tie!!");
+//                 break;
+//             case roundResult === "You lose, Rock beats Scissors!!":
+//                 computerCounter++;
+//                 break;
+//             case roundResult === "You lose, Paper beats Rock!!":
+//                 computerCounter++;
+//                 break;
+//             case roundResult === "You Lose, Scissors beats Paper!!":
+//                 computerCounter++;
+//                 break;
+//             case roundResult === "You Win, Scissors beats Paper!!":
+//                 playerCounter++;
+//                 break;
+//             case roundResult === "You Win, Paper beats Rock!!":
+//                 playerCounter++;
+//                 break;
+//             case roundResult === "You Win, Rock beats Scissors!!":
+//                 playerCounter++;
+//                 break;
+//             default:
+//                 console.log("Nothing to show");
+//         }
+//     // }
+//     // gameCounter(roundResult);
+//     // console.log(playerCounter, computerCounter);
+//     // if (computerCounter === 5) {
+//     //     console.log("Has perdido manco!!");
+//     // } else if (playerCounter === 5) {
+//     //     console.log("Eres genial!!!");
+//     }
+// }
